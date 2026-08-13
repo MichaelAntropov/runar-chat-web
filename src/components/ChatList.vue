0c37bb9a8d0e67e80e0c8bcfe5d6ca918486b66b
Mykhailo Antropov <m.antropov@abc-mobile.eu>
2026-08-14 01:53:55 +0300

    Chat with the most recent messages goes on top of the list

--- src/components/ChatList.vue
+++ src/components/ChatList.vue
@@ -1,14 +1,25 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+
 import { useChatsStore } from '@/chat/chatStore'
+
 import ChatListItem from './ChatListItem.vue'
 
 const emit = defineEmits(['open-chat'])
 const chatStore = useChatsStore()
+
+const sortedChats = computed(() =>
+  [...chatStore.chats].sort(
+    (firstChat, secondChat) =>
+      (secondChat.lastMessageTime ?? Number.NEGATIVE_INFINITY) -
+      (firstChat.lastMessageTime ?? Number.NEGATIVE_INFINITY),
+  ),
+)
 </script>
 
 <template>
   <div class="list-group list-group-flush border-bottom overflow-y-auto">
-    <template v-for="chat in chatStore.chats" :key="chat.id">
+    <template v-for="chat in sortedChats" :key="chat.id">
       <ChatListItem :chat="chat" @click="emit('open-chat')" />
     </template>
   </div>
